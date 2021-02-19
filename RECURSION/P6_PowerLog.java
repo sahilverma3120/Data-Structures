9502222b3a02dc44263e4dd94f4fb7e99b7c0dba
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-19 23:26:18 +0530

    Create P6_PowerLog.java

---
+++ RECURSION/P6_PowerLog.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        int xn = power(x,n);
+        System.out.println(xn);
+    }
+
+    public static int power(int x, int n){
+    if(n==0)
+    {
+        return 1; 
+    }
+      
+    int fhn2=power(x,n/2);
+    int xn=fhn2 * fhn2;
+    
+     if(n%2==1){
+     xn=xn *x;
+     }
+      return xn;
+     
+
+    
+    
+    }
+
+}
