92847a72537634641b444e6001a5eea52fdb3e4b
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-23 23:38:57 +0530

    Update removenode.cpp

--- bst/removenode.cpp
+++ bst/removenode.cpp
@@ -85,8 +85,47 @@ public class Main {
     display(node.right);
   }
 
+public static int max(Node node){
+    if(node.right != null){
+        return max(node.right);
+    }
+    else{
+        return node.data;
+    }
+}
+
   public static Node remove(Node node, int data) {
-    // write your code here
+      if(node == null){
+          return null;
+      }
+    if(data > node.data){
+        node.right = remove(node.right,data);
+    }
+    else if(data < node.data){
+        node.left = remove(node.left, data);
+        
+    }
+
+else {
+  if(node.left != null && node.right != null){
+  int lmax = max(node.left);
+  node.data = lmax;
+  node.left = remove(node.left,lmax);
+  return node;
+ 
+  } 
+
+   else if(node.left != null){
+       return node.left;
+   } 
+   else if(node.right != null){
+       return node.right;
+   }
+   else {
+       return null;
+   }
+}    
+return node;
   }
 
   public static void main(String[] args) throws Exception {
