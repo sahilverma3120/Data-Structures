ed0de5aed6ab22ea79f0c36c76ede7bf0ac4484b
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-23 23:37:30 +0530

    Create removenode.cpp

---
+++ bst/removenode.cpp
@@ -0,0 +1,5 @@
+1. You are given a partially written BST class.
+2. You are required to complete the body of remove function. "remove" function is expected to remove a new node with given data to the tree and return the new root.
+3. Input and Output is managed for you. 
+
+Note -> Please watch the question video to figure out the algorithm required for deletion. It specifies some requirements of the question as well.
