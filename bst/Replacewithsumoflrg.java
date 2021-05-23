f340186b107476b6a3b40d774b65ba70c42925e8
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-23 23:39:56 +0530

    Create Replacewithsumoflrg.java

---
+++ bst/Replacewithsumoflrg.java
@@ -0,0 +1,5 @@
+/*1. You are given a partially written BST class.
+2. You are required to complete the body of rwsol function. "rwsol" function is expected to replace a node's value with sum of all nodes greater than it.
+3. Input and Output is managed for you. 
+
+Note -> Please watch the question video for clarity. Use the statis sum data member to complete your code.*/
