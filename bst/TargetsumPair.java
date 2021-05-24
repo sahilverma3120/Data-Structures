a9f85cd7e03ce3f9a5b1eab1f401d0db497374b3
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-24 23:07:23 +0530

    Update TargetsumPair.java

--- bst/TargetsumPair.java
+++ bst/TargetsumPair.java
@@ -1,4 +1,7 @@
-/*import java.io.*;
+/*1. You are given a partially written BST class.
+2. You are given a value. You are required to print all pair of nodes which add up to the given value. Make sure all pairs print the smaller value first and avoid duplicacies. Make sure to print the pairs in increasing order. Use the question video to gain clarity.
+3. Input and Output is managed for you. */
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -98,4 +101,4 @@ public class Main {
     // write your code here
   }
 
-}*/
+}
