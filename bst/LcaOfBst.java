ec14dabe1e5505a3b363e80c849fc208d7c4d75d
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-26 20:36:38 +0530

    Create LcaOfBst.java

---
+++ bst/LcaOfBst.java
@@ -0,0 +1,5 @@
+1. You are given a partially written BST class.
+2. You are required to complete the body of lca function. "lca" function is expected to find the lowest commong ancestor of d1 and d2.
+3. Input and Output is managed for you. 
+
+Note -> Please watch the question video for clarity.
