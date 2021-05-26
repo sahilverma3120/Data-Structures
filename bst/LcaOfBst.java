1473a028536b76cb66a40b97b2fe36688e3826c8
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-26 20:37:37 +0530

    Update LcaOfBst.java

--- bst/LcaOfBst.java
+++ bst/LcaOfBst.java
@@ -1,4 +1,5 @@
-/*1. You are given a partially written BST class.
+/*Link:-https://www.pepcoding.com/resources/online-java-foundation/binary-search-tree/lca-bst-official/ojquestion
+1. You are given a partially written BST class.
 2. You are required to complete the body of lca function. "lca" function is expected to find the lowest commong ancestor of d1 and d2.
 3. Input and Output is managed for you. 
 
