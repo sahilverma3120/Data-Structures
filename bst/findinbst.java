0b97b733080a0741047b8644dac24808f97c29c3
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-27 20:41:30 +0530

    Update and rename findinbst to findinbst.java

--- bst/findinbst.java
+++ bst/findinbst.java
@@ -1,4 +1,4 @@
-1. You are given a partially written BST class.
+/*1. You are given a partially written BST class.
 2. You are required to complete the body of size, sum, max, min and find function. The functions are expected to:
     2.1. size - return the number of nodes in BST
     2.2. sum - return the sum of nodes in BST
@@ -6,3 +6,4 @@
     2.4. min - return the value of node with smallest value in BST
     2.5. find - return true if there is node in the tree equal to "data"
 3. Input and Output is managed for you.
+*/
