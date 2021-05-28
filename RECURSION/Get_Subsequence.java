cf86314833cd7df47fdf07c4490069c29d55e85d
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-28 19:31:42 +0530

    Update Get_Subsequence.java

--- RECURSION/Get_Subsequence.java
+++ RECURSION/Get_Subsequence.java
@@ -1,44 +1,6 @@
-Maintain 2 pointers 'p1' and 'p2' denoting the left and right endpoints of a sub-array.
-Initialize p1, p2 = 0.
-Start moving p2 forward till a repetition is found. Maintain a set of elements in the window to figure out repetition.
-As soon as a repetition is found, start contracting the window by moving the left pointer p1 forward. Reduce the elements from the sum and the set. Also store it in max if the sum is larger than the previously found maximum sum which is stored in max.
-Keep doing this till p2 reaches the end of the array.
-In the end, compare with 'max' as the running sum of the last window nay be more than the max found til now.
-Return max as result
-/*
-Algorithm:
-1. Maintain 2 pointers 'p1' and 'p2' denoting the left and right endpoints of a sub-array.
-2. Initialize p1, p2 = 0.
-3. Start moving p2 forward till a repetition is found. Maintain a set of elements in the window to figure out repetition.
-4. As soon as a repetition is found, start contracting the window by moving the left pointer p1 forward. Reduce the elements from the sum and the set. Also store it in max if the sum is larger than the previously found maximum sum which is stored in max.
-5. Keep doing this till p2 reaches the end of the array.
-6. In the end, compare with 'max' as the running sum of the last window nay be more than the max found til now.
-7. Return max as result
-*/
-class Solution {
-    public int maximumUniqueSubarray(int[] nums) {
-        int p1 = 0;
-        int p2 = 0;
-        int sum = 0;
-        int max = 0;
-        Set<Integer> set = new HashSet<>();
-        while(p2 < nums.length) {
-            if(set.contains(nums[p2])) {
-                max = Math.max(max, sum);
-                while(nums[p1] != nums[p2]) {
-                    set.remove(nums[p1]);
-                    sum = sum - nums[p1];
-                    p1++;
-                }
-                set.remove(nums[p1]);
-                sum = sum - nums[p1];
-                p1++;
-            } 
-            set.add(nums[p2]);
-            sum = sum + nums[p2];
-            p2++;
-        }
-        max = Math.max(max, sum);
-        return max;
-    }
-}
+/*1. You are given a string str.
+2. Complete the body of getSS function - without changing signature - to calculate all subsequences of str.
+Use sample input and output to take idea about subsequences.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.
+Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.*/
