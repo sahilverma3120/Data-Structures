2fbac9ada0c61b8bd6f7f838fb7947098ccdcc11
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-28 19:32:49 +0530

    Update Get_Subsequence.java

--- RECURSION/Get_Subsequence.java
+++ RECURSION/Get_Subsequence.java
@@ -11,13 +11,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-
+       
+    Scanner scn = new Scanner(System.in);
+    String str= scn.next();
+    ArrayList<String> res = gss(str);
+    System.out.println(res);
+       
+       
     }
 
     public static ArrayList<String> gss(String str) {
-        return null;
-    }
+       
+       if(str.length()==0){
+           ArrayList<String> bres = new ArrayList<>();
+           bres.add("");
+           return bres;
+       }
+       char ch = str.charAt(0);
+       String ros= str.substring(1);
+       ArrayList<String> rres = gss(ros);
+       
+       ArrayList<String>mres= new ArrayList<>();
+       for(String rstr:rres){
+           mres.add(""+ rstr);
+       }
+       for(String rstr:rres){
+           mres.add(ch+rstr);
+       }
+       
+return mres;
+       
+       }
+       
+       
 
-}
+    }
