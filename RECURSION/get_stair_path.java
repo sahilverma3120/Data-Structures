1b9171c2c43845f00b8c15c3d5a0587636ca6449
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-28 23:15:46 +0530

    Update get_stair_path.java

--- RECURSION/get_stair_path.java
+++ RECURSION/get_stair_path.java
@@ -11,39 +11,40 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
+        int n = scn.nextInt();
         ArrayList<String> paths = getStairPaths(n);
         System.out.println(paths);
 
     }
 
     public static ArrayList<String> getStairPaths(int n) {
-        
-        
+        if(n==0){
+        ArrayList<String> bres = new ArrayList<>();
+        bres.add("");
+        return bres;}
+        else if(n<0){
+            ArrayList<String> bres = new ArrayList<>();
+            return bres;
+        }
         ArrayList<String> paths1=getStairPaths(n-1);
         ArrayList<String> paths2=getStairPaths(n-2);
         ArrayList<String> paths3=getStairPaths(n-3);
         
-        ArrayList<string> paths = new ArrayList<>();
+        ArrayList<String> paths = new ArrayList<>();
         
-        for(string path:paths1)
+        for(String path:paths1)
         {
             paths.add(1+path);
         }
-        for(string path:paths2
+        for(String path:paths2)
         {
             paths.add(2+path);
         }
-         for(string path:paths2
+         for(String path:paths3)
         {
             paths.add(3+path);
         }
-        
-        
-        
-        
-        
-        return paths
+        return paths;
     }
 
 }
