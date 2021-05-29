21787bf8fab4a7421251c655185f93de98e56c7f
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-29 22:21:30 +0530

    Update print_stair_paths.java

--- RECURSION/print_stair_paths.java
+++ RECURSION/print_stair_paths.java
@@ -2,5 +2,18 @@
 2. You are standing at the bottom of staircase. You are allowed to climb 1 step, 2 steps or 3 steps in one move.
 3. Complete the body of printStairPaths function - without changing signature - to print the list of all paths that can be used to climb the staircase up.
 Use sample input and output to take idea about output.
-
+link- https://www.pepcoding.com/resources/online-java-foundation/recursion-on-the-way-up/print-stair-paths-official/ojquestion
 Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static void printStairPaths(int n, String path) {
+    }
+
+}
