25433e71225ec4a75776ec631b140fcff656c52b
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-01 19:17:17 +0530

    Update hasPath.java

--- graph/hasPath.java
+++ graph/hasPath.java
@@ -1,3 +1,7 @@
+/*https://www.pepcoding.com/resources/online-java-foundation/graphs/has-path-official/ojquestion*/
+/*1. You are given a graph, a src vertex and a destination vertex.
+2. You are required to find if a path exists between src and dest. If it does, print true 
+     otherwise print false.*/
 import java.io.*;
 import java.util.*;
 
