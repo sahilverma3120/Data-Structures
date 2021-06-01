5fb83cdde5669873e94af57b8dc45c1005b6856e
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-01 19:18:05 +0530

    Update isgraphconn.java

--- graph/isgraphconn.java
+++ graph/isgraphconn.java
@@ -1,3 +1,8 @@
+/*1. You are given a graph.
+2. You are required to find and print if the graph is connected (there is a path from 
+     every vertex to every other).
+*/
+//https://www.pepcoding.com/resources/online-java-foundation/graphs/is-graph-connected-official/ojquestion
 import java.io.*;
 import java.util.*;
 
