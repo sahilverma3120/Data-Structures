1e9e568ca5833117a31d4f7886a4650dba0be7c4
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-01 19:16:21 +0530

    Update conncomp.java

--- graph/conncomp.java
+++ graph/conncomp.java
@@ -1,3 +1,7 @@
+/*1. You are given a graph.
+2. You are required to find and print all connected components of the graph.*/
+//https://www.pepcoding.com/resources/online-java-foundation/graphs/gcc-official/ojquestion
+
 import java.io.*;
 import java.util.*;
 
