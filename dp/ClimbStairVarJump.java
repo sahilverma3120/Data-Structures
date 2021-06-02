f1e855a9ef3cdd5af492333007a531986af5c727
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-02 20:50:11 +0530

    Update ClimbStairVarJump.java

--- dp/ClimbStairVarJump.java
+++ dp/ClimbStairVarJump.java
@@ -1,6 +1,7 @@
-1. You are given a number n, representing the number of stairs in a staircase.
+link - https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/climb-stairs-with-variable-jumps-official/ojquestion
+/*1. You are given a number n, representing the number of stairs in a staircase.
 2. You are on the 0th step and are required to climb to the top.
 3. You are given n numbers, where ith element's value represents - till how far from the step you 
      could jump to in a single move.  
      You can of course jump fewer number of steps in the move.
-4. You are required to print the number of different paths via which you can climb to the top.
+4. You are required to print the number of different paths via which you can climb to the top.*/
