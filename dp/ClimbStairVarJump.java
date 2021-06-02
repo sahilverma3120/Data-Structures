a0d53dbae56b49a0595e5aba9e1a61e8e9613619
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-02 20:59:14 +0530

    AC ALL

--- dp/ClimbStairVarJump.java
+++ dp/ClimbStairVarJump.java
@@ -16,7 +16,7 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for(int i=0; i < arr.length; i++)
+        for(int i=0; i <arr.length; i++)
         {
           arr[i]=scn.nextInt();  
         }
@@ -25,7 +25,7 @@ public class Main {
         dp[n]=1;
         for(int i = n-1; i>=0; i--)
         {
-            for(int j=1;j<=arr[i] && i+j<=dp.length;j++){
+            for(int j=1;j<=arr[i] && i+j<dp.length;j++){
                 dp[i]+=dp[i+j];
             }
         }
