c36108fa97bd82edbbd8862d67b60c9698e8c619
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-03 20:25:56 +0530

    Update selectionsort.java

--- Sorting And Searching/selectionsort.java
+++ Sorting And Searching/selectionsort.java
@@ -1,4 +1,4 @@
-https://www.pepcoding.com/resources/online-java-foundation/time-and-space-complexity/selection-sort-official/ojquestion
+//https://www.pepcoding.com/resources/online-java-foundation/time-and-space-complexity/selection-sort-official/ojquestion
 /*1. You are given an array(arr) of integers.
 2. You have to sort the given array in increasing order using selection sort.
 Input Format
@@ -7,4 +7,50 @@ arr1
 arr2..
 n integers
 Output Format
-Check the sample ouput and question video.
+Check the sample ouput and question video.*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void selectionSort(int[] arr) {
+    //write your code here
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if ith element is smaller than jth element
+  public static boolean isSmaller(int[] arr, int i, int j) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    selectionSort(arr);
+    print(arr);
+  }
+
+}
