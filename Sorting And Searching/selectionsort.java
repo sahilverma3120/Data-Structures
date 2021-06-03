623d0fc208f0fb27296150095524a8097bb5abf5
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-03 20:25:31 +0530

    Update selectionsort.java

--- Sorting And Searching/selectionsort.java
+++ Sorting And Searching/selectionsort.java
@@ -1,5 +1,5 @@
 https://www.pepcoding.com/resources/online-java-foundation/time-and-space-complexity/selection-sort-official/ojquestion
-1. You are given an array(arr) of integers.
+/*1. You are given an array(arr) of integers.
 2. You have to sort the given array in increasing order using selection sort.
 Input Format
 An Integer n 
