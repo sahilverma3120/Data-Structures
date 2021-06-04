3d43ed6206af65e5f5d161964d968885d1de4ca4
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-04 23:27:53 +0530

    Create insertsort.cpp

---
+++ Sorting And Searching/insertsort.cpp
@@ -0,0 +1,57 @@
+//link https://www.pepcoding.com/resources/online-java-foundation/time-and-space-complexity/insertion-sort-official/ojquestion
+/*1. You are given an array(arr) of integers.
+2. You have to sort the given array in increasing order using insertion sort.
+Input Format
+An Integer n 
+arr1
+arr2..
+n integers
+Output Format
+Check the sample output and question video.*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void insertionSort(int[] arr) {
+    //write your code here
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if jth element is greater than ith element
+  public static boolean isGreater(int[] arr, int j, int i) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    insertionSort(arr);
+    print(arr);
+  }
+
+}
