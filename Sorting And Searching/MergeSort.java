6b49df24365f650a9c5c634e270e509393636479
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-04 23:30:14 +0530

    Create MergeSort.java

---
+++ Sorting And Searching/MergeSort.java
@@ -0,0 +1,9 @@
+/*1. You are given an array(arr) of integers.
+2. You have to sort the given array in increasing order using the merge sort.
+Input Format
+An Integer n 
+arr1
+arr2..
+n integers
+Output Format
+Check the sample output and question video.*/
