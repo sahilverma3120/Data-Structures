8f18a55d80cf91a7b428abe570b62a6a4afdc693
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-05 22:59:49 +0530

    Create ArrayPartitiom.java

---
+++ Sorting And Searching/ArrayPartitiom.java
@@ -0,0 +1,11 @@
+1. You are given an array(arr) of integers and a pivot.
+2. You have to re-arrange the given array in such a way that all elements smaller or equal to pivot lie on the left side of pivot and all elements greater than pivot lie on its right side.
+3. You have to achieve this in linear time.
+
+Note -> For more information, watch question video.
+Input Format
+An Integer n 
+arr1
+arr2..
+n integers
+An integer pivot
