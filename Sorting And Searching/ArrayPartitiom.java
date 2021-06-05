add62dc6590f479d48da224a042a8ec807d22474
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-05 23:00:46 +0530

    Update ArrayPartitiom.java

--- Sorting And Searching/ArrayPartitiom.java
+++ Sorting And Searching/ArrayPartitiom.java
@@ -1,4 +1,5 @@
-1. You are given an array(arr) of integers and a pivot.
+//link https://www.pepcoding.com/resources/online-java-foundation/time-and-space-complexity/partition-an-array-official/ojquestion
+/*1. You are given an array(arr) of integers and a pivot.
 2. You have to re-arrange the given array in such a way that all elements smaller or equal to pivot lie on the left side of pivot and all elements greater than pivot lie on its right side.
 3. You have to achieve this in linear time.
 
@@ -8,4 +9,4 @@ An Integer n
 arr1
 arr2..
 n integers
-An integer pivot
+An integer pivot*/
