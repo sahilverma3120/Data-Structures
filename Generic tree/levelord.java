d021fd178ac159bdb3836c8c7358f5f1c4cffd38
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-06 19:32:17 +0530

    Update levelord.java

--- Generic tree/levelord.java
+++ Generic tree/levelord.java
@@ -1,1 +1,6 @@
-linewise
+//linewise
+/*1. You are given a partially written GenericTree class.
+2. You are required to complete the body of levelorderLineWise function. The function is expected to visit every node in "levelorder fashion" and print them from left to right (level by level). All nodes on same level should be separated by a space. Different levels should be on separate lines. Please check the question video for more details.
+3. Input is managed for you.*/
+
+https://www.pepcoding.com/resources/online-java-foundation/generic-tree/levelorder-linewise-generic-tree-official/ojquestion
