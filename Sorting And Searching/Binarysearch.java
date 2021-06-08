653e17b7c7c799a640f8bebb03e4b5c04b3f9965
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-08 22:55:54 +0530

    Update Binarysearch.java

--- Sorting And Searching/Binarysearch.java
+++ Sorting And Searching/Binarysearch.java
@@ -23,7 +23,22 @@ nums is sorted in an ascending order.
 */
 
 class Solution {
-    public int search(int[] nums, int target) {
+       public int search(int[] nums, int target) {
+        int left    = 0;
+        int right   = nums.length-1;
         
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            
+            if (target < nums[middle]) {
+                right = middle - 1;
+            } else if (target > nums[middle]) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        
+        return -1;
     }
 }
