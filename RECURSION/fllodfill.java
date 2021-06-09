236a40e545f34bc29ef24be64d48fe26baf9f4f7
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-09 23:59:40 +0530

    Update fllodfill.java

--- RECURSION/fllodfill.java
+++ RECURSION/fllodfill.java
@@ -12,21 +12,40 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
+        // write your code here
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
         int[][] arr = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = scn.nextInt();
+        for(int i = 0; i<arr.length;i++){
+            for(int j = 0; j<arr[0].length;j++)
+            {
+                arr[i][j]=scn.nextInt();
             }
         }
-        floodfill(arr, 0, 0, "");
+        boolean[][] visited=  new boolean[n][m];
+        floodfill(arr,0,0,"",visited);
+        
     }
-    
-    // asf -> answer so far
-    public static void floodfill(int[][] maze, int sr, int sc, String asf) {
-    
+
+    public static void floodfill(int[][] maze, int row, int col, String psf, boolean[][] visited){
+    if(row <0 || col<0 || row==maze.length || col==maze[0].length || maze[row][col]==1 || visited[row][col]==true;
+    return;        
+    }
+    if(row==maze.length-1 || col==maze[0].length-1)
+    {
+        System.out.println(psf);
+        return;
+    }
+    visited[row][col]=true;
+    floodfill(maze,row-1,col,psf+"t",visited);
+    floodfill(maze,row,col+1,psf+"l",visited);
+    floodfill(maze,row+1,col,psf+"d",visited);
+    floodfill(maze,row,col+1,psf+"r",visited);
+     visited[row][col]=false;
+   
+        
     }
 }
