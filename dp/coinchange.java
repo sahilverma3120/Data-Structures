a1c75fc6a2d4da593e7b474b8f4537483fcc0993
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-12 23:29:53 +0530

    Create coinchange.java

---
+++ dp/coinchange.java
@@ -0,0 +1,11 @@
+1. You are given a number n, representing the count of coins.
+2. You are given n numbers, representing the denominations of n coins.
+3. You are given a number "amt".
+4. You are required to calculate and print the number of combinations of the n coins using which the 
+     amount "amt" can be paid.
+
+Note1 -> You have an infinite supply of each coin denomination i.e. same coin denomination can be 
+                  used for many installments in payment of "amt"
+Note2 -> You are required to find the count of combinations and not permutations i.e.
+                  2 + 2 + 3 = 7 and 2 + 3 + 2 = 7 and 3 + 2 + 2 = 7 are different permutations of same 
+                  combination. You should treat them as 1 and not 3.
