d39bbbd24268433b354f55b702062e3e26ab798f
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-12 23:30:35 +0530

    Update coinchange.java

--- dp/coinchange.java
+++ dp/coinchange.java
@@ -1,4 +1,4 @@
-1. You are given a number n, representing the count of coins.
+/*1. You are given a number n, representing the count of coins.
 2. You are given n numbers, representing the denominations of n coins.
 3. You are given a number "amt".
 4. You are required to calculate and print the number of combinations of the n coins using which the 
@@ -9,3 +9,13 @@ Note1 -> You have an infinite supply of each coin denomination i.e. same coin de
 Note2 -> You are required to find the count of combinations and not permutations i.e.
                   2 + 2 + 3 = 7 and 2 + 3 + 2 = 7 and 3 + 2 + 2 = 7 are different permutations of same 
                   combination. You should treat them as 1 and not 3.
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
