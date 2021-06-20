c4c75e578a3cdfe4a56f8a878c25effeedb5e414
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-20 23:17:04 +0530

    Create StringWithdiff.java

---
+++ strings/StringWithdiff.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String solution(String str){
+		// write your code here
+
+		return null;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
+}
