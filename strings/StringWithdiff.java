4eea1e6e679bff44abc6b74739beb9d8971de34a
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-20 23:17:29 +0530

    Update StringWithdiff.java

--- strings/StringWithdiff.java
+++ strings/StringWithdiff.java
@@ -1,3 +1,11 @@
+/*1. You are given a string that contains only lowercase and uppercase alphabets. 
+2. You have to form a string that contains the difference of ASCII values of every two consecutive characters between those characters.
+   For "abecd", the answer should be "a1b3e-2c1d", as 
+   'b'-'a' = 1
+   'e'-'b' = 3
+   'c'-'e' = -2
+   'd'-'c' = 1*/
+
 import java.io.*;
 import java.util.*;
 
