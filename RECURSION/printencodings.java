abe466dd49466fe3b073040e3edadaed33d2e12d
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-22 22:33:46 +0530

    Update printencodings.java

--- RECURSION/printencodings.java
+++ RECURSION/printencodings.java
@@ -1,1 +1,15 @@
 https://www.pepcoding.com/resources/online-java-foundation/recursion-on-the-way-up/print-encodings-official/ojquestion
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static void printEncodings(String str) {
+        
+    }
+
+}
