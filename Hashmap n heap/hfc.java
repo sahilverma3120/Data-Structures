f645fb699526fafc87a4445080312baa02308e00
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-23 20:45:48 +0530

    Update hfc.java

--- Hashmap n heap/hfc.java
+++ Hashmap n heap/hfc.java
@@ -11,7 +11,32 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // write your code here
+        
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0; i< str.length(); i++){
+            char ch = str.charAt(i);
+            if(hm.containsKey(ch)){
+                int of = hm.get(ch);
+                int nf = of +1;
+                hm.put(ch, nf);
+            }
+            else{
+                hm.put(ch,1);
+            }
+        }
+ 
+        char mfc = str.charAt(0);
+        for(Character key : hm.keySet()){
+            if(hm.get(key) > hm.get(mfc)){
+                mfc = key;
+                
+            }
+            
+        }
+                    System.out.println(mfc);
     }
 
 }
