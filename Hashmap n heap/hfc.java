1c83c7b5a74d644a613e2249b351fda43e4285ea
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-23 20:44:51 +0530

    Create hfc.java

---
+++ Hashmap n heap/hfc.java
@@ -0,0 +1,17 @@
+1. You are given a string str.
+2. You are required to find the character with maximum frequency.
+Input Format
+A string str
+Output Format
+The character with highest frequency
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+}
