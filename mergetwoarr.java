c7ae15d47c258f3f920efd4a888fdbcca81b9121
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-24 22:54:24 +0530

    Create mergetwoarr.java

---
+++ mergetwoarr.java
@@ -0,0 +1,9 @@
+Sum Of Two Arrays
+Easy  Prev   Next
+1. You are given a number n1, representing the size of array a1.
+2. You are given n1 numbers, representing elements of array a1.
+3. You are given a number n2, representing the size of array a2.
+4. You are given n2 numbers, representing elements of array a2.
+5. The two arrays represent digits of two numbers.
+6. You are required to add the numbers represented by two arrays and print the
+arrays.
