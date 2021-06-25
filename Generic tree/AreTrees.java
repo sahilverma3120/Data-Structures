2c33489514d8b14669d1ecaa6e09204518983c2e
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-25 23:38:47 +0530

    Update AreTrees.java

--- Generic tree/AreTrees.java
+++ Generic tree/AreTrees.java
@@ -80,7 +80,19 @@ public class Main {
   }
 
   public static boolean areSimilar(Node n1, Node n2) {
-    // write your code here
+  
+   if(n1.children.size() != n2.children.size()){
+       return false;
+   }
+  for(int i=0; i<n1.children.size(); i++){
+      Node c1 = n1.children.get(i);
+      Node c2 = n2.children.get(i);
+      if(areSimilar(c1,c2)==false){
+          return false;
+      }
+  }
+  
+  return true;
   }
 
   public static void main(String[] args) throws Exception {
