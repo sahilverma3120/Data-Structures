784cd852f12de447312dcc93f50e870491053b19
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-27 23:44:21 +0530

    Update and rename AddNodetobst to AddNodetobst.java

--- bst/AddNodetobst.java
+++ bst/AddNodetobst.java
@@ -1,3 +1,7 @@
+/*1. You are given a partially written BST class.
+2. You are required to complete the body of add function. "add" function is expected to add a new node with given data to the tree and return the new root.
+3. Input and Output is managed for you.*/
+
 import java.io.*;
 import java.util.*;
 
