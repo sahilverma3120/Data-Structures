51a1c078e3154b845fdf65763f0613d3ad1ce4a1
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-28 23:45:54 +0530

    Update allindi.java

--- RECURSION/allindi.java
+++ RECURSION/allindi.java
@@ -26,6 +26,21 @@ public class Main {
 
     public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
         // write ur code here
+        if(idx==arr.length){
+            return  new int[fsf];
+        }
+        
+    if(arr[idx]==x)
+    {
+        int[] iarr = allIndices(arr,x,idx+1,fsf+1);
+        iarr[fsf]=idx;
+        return iarr;
+    }
+    else{
+        int[] iarr = allIndices(arr,x,idx+1,fsf);
+        return iarr;
+    }
+        
     }
 
 }
