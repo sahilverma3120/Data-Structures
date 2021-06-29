d28ec0a2493b3d81a70c0d4fa34cfe6c58185e77
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-06-29 20:29:10 +0530

    Update fllodfill.java

--- RECURSION/fllodfill.java
+++ RECURSION/fllodfill.java
@@ -36,7 +36,7 @@ public class Main {
     if(row <0 || col<0 || row==maze.length || col==maze[0].length || maze[row][col]==1 || visited[row][col]==true){
     return;        
     }
-    if(row==maze.length-1 || col==maze[0].length-1)
+    if(row==maze.length-1 && col==maze[0].length-1)
     {
         System.out.println(psf);
         return;
