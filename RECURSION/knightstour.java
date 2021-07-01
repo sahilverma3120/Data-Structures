a6f7c64e7d559b63b6c88511202b303121832ad9
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-07-01 22:27:06 +0530

    Update knightstour.java

--- RECURSION/knightstour.java
+++ RECURSION/knightstour.java
@@ -1,4 +1,4 @@
-1. You are given a number n, the size of a chess board.
+/*1. You are given a number n, the size of a chess board.
 2. You are given a row and a column, as a starting point for a knight piece.
 3. You are required to generate the all moves of a knight starting in (row, col) such that knight visits 
      all cells of the board exactly once.
@@ -11,4 +11,28 @@ Note -> When moving from (r, c) to the possible 8 options give first precedence 
                explore other options.
 Note -> The online judge can't force you to write the function recursively but that is what the spirit of 
                question is. Write recursive and not iterative logic. The purpose of the question is to aid 
-               learning recursion and not test you.
+               learning recursion and not test you.*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+        
+    }
+
+    public static void displayBoard(int[][] chess){
+        for(int i = 0; i < chess.length; i++){
+            for(int j = 0; j < chess[0].length; j++){
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
