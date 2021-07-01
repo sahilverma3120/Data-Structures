7b8566c914ead41e1317479ebff0e728190d13df
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-07-01 22:28:06 +0530

    Update knightstour.java

--- RECURSION/knightstour.java
+++ RECURSION/knightstour.java
@@ -18,11 +18,35 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int r = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        int[][] chess = new int[n][n];
+        printKnightsTour(chess, r, c, 1);
     }
 
-    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
-        
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove){
+        if(r < 0 || c < 0 || r >= chess.length || c >= chess.length || chess[r][c] != 0){
+            return;
+        } else if(upcomingMove == chess.length * chess.length){
+            chess[r][c] = upcomingMove;
+            displayBoard(chess);
+            chess[r][c] = 0;    
+            return;
+        }
+
+        chess[r][c] = upcomingMove;
+        printKnightsTour(chess, r - 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c + 2 , upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c + 2 , upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c + 1 , upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c - 1 , upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c - 2 , upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c - 2 , upcomingMove + 1);
+        printKnightsTour(chess, r - 2, c - 1 , upcomingMove + 1);
+        chess[r][c] = 0;
     }
 
     public static void displayBoard(int[][] chess){
@@ -36,3 +60,10 @@ public class Main {
         System.out.println();
     }
 }
+
+
+                        
+
+
+                        
+                        
