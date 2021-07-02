81e7822d61e67ef4c058e63d851dc37777b93b0b
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-07-02 23:14:36 +0530

    Update arrrotim.java

--- arrrotim.java
+++ arrrotim.java
@@ -1,3 +1,9 @@
+
+/*You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.*/
+
+
 class Solution {
     public void rotate(int[][] matrix) {
         transpose(matrix);
