d8e8e683045578562467c4a1202dc9e09dd0800d
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-07-07 22:53:49 +0530

    Update ReduceArrtoHalf.java

--- ReduceArrtoHalf.java
+++ ReduceArrtoHalf.java
@@ -1,1 +1,32 @@
 
+class Solution {
+    public int minSetSize(int[] arr) {
+        int res = 0;
+        Arrays.sort(arr); //nlog(n)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1]-a[1]); 
+        int count = 1;
+        int prev = arr[0];
+        for(int i = 1;i<arr.length;i++){  //O(n)
+            int cur = arr[i];
+            if(prev!=cur){
+                int[] temp ={prev,count};
+                pq.add(temp); //logn
+                count = 1;
+                prev = cur;
+            }else{
+                count++;
+            }
+        }
+        if(count>0){
+            int[] temp ={prev,count};
+            pq.add(temp);
+        }
+        int tot = arr.length;
+        while(tot>arr.length/2){
+            int freq = pq.poll()[1];
+            tot -= freq;
+            res++;
+        }
+        return res;
+    }
+}
