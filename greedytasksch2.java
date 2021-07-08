ab9da4a681cbe6daea07f8d10317f47fb4e206ed
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-07-08 15:35:24 +0530

    Update greedytasksch2.java

--- greedytasksch2.java
+++ greedytasksch2.java
@@ -1,1 +1,26 @@
 
+
+class Solution {
+public int leastInterval(char[] tasks, int n) {
+    int[] count = new int[26];
+    for(char c : tasks){
+        count[c - 'A']++;
+    }
+    
+    int maxFreq = 0;
+    int groupSize = 0;
+    
+    for(int i : count){
+        if(i > maxFreq){
+            maxFreq = i;
+            groupSize = 0;
+        }
+        if(maxFreq == i){
+            groupSize++;
+        }
+    }
+    
+    return Math.max(tasks.length, (n + 1) * (maxFreq - 1) + groupSize);
+    
+    }
+}
