037593bd8de2603a80a4bed6a6da8c8a4d3da6a6
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-07-08 15:34:34 +0530

    Update greedytaskschesol1.java

--- greedytaskschesol1.java
+++ greedytaskschesol1.java
@@ -1,1 +1,24 @@
 
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        // frequencies of the tasks
+        int[] frequencies = new int[26];
+        for (int t : tasks) {
+            frequencies[t - 'A']++;
+        }
+
+        Arrays.sort(frequencies);
+
+        // max frequency
+        int f_max = frequencies[25];
+        int idle_time = (f_max - 1) * n;
+        
+        for (int i = frequencies.length - 2; i >= 0 && idle_time > 0; --i) {
+            idle_time -= Math.min(f_max - 1, frequencies[i]); 
+        }
+        idle_time = Math.max(0, idle_time);
+
+        return idle_time + tasks.length;
+    }
+}
