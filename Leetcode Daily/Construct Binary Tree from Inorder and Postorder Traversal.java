d9fc4d161d9d06db28018c65045234aea5bd4677
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-11-22 23:49:20 +0530

    Update Construct Binary Tree from Inorder and Postorder Traversal.java

--- Leetcode Daily/Construct Binary Tree from Inorder and Postorder Traversal.java
+++ Leetcode Daily/Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -13,31 +13,29 @@
  *     }
  * }
  */
-class Solution {
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (postorder.length == 0 && inorder.length == 0)
-            return null;
-        if (postorder.length == 1 && inorder.length == 1)
-            return new TreeNode(inorder[0]);
-        int len = postorder.length;
-        int root = postorder[len - 1];
-        int index = findIndex(inorder, root);
-        
-        int[] leftIn = Arrays.copyOfRange(inorder, 0, index);
-        int[] rightIn = Arrays.copyOfRange(inorder, index + 1, len);
-        
-        int[] leftPost = Arrays.copyOfRange(postorder, 0, index);
-        int[] rightPost = Arrays.copyOfRange(postorder, index, len - 1);
-        
-        TreeNode lroot = buildTree(leftIn, leftPost);
-        TreeNode rroot = buildTree(rightIn, rightPost);
-        
-        return new TreeNode(root, lroot, rroot);
-    }
-    int findIndex(int[] arr, int x) {
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i] == x)
-                return i;
-        return -1;
-    }
-}
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+Example 3:
+
+Input: root = [], key = 0
+Output: []
