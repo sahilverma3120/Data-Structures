b5c7dd321e0a446d25371b6008f701a072e46c28
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-11-23 21:27:36 +0530

    Update 952. Largest Component Size by Common Factor.java

--- 952. Largest Component Size by Common Factor.java
+++ 952. Largest Component Size by Common Factor.java
@@ -1,12 +1,3 @@
-//952. Largest Component Size by Common Factor
-/*
-You are given an integer array of unique positive integers nums. Consider the following graph:
-
-There are nums.length nodes, labeled nums[0] to nums[nums.length - 1],
-There is an undirected edge between nums[i] and nums[j] if nums[i] and nums[j] share a common factor greater than 1.
-Return the size of the largest connected component in the graph.
-*/
-
 class Solution {
     public int find(int x,int[] parent)
     {
@@ -47,7 +38,7 @@ class Solution {
         {
             int p=find(nums[i],parent);
             count=Math.max(count,map.getOrDefault(p,0)+1);
-            map.put(p,map.getOrDefault(p,0));
+            map.put(p,map.getOrDefault(p,0)+1);
         }
         return count;
     }
