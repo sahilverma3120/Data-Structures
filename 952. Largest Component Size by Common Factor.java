56935bd9f80003fc5b68e1a490fe7c2f970cdaa6
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-11-23 21:25:07 +0530

    Create 952. Largest Component Size by Common Factor.java

---
+++ 952. Largest Component Size by Common Factor.java
@@ -0,0 +1,8 @@
+//952. Largest Component Size by Common Factor
+/*
+You are given an integer array of unique positive integers nums. Consider the following graph:
+
+There are nums.length nodes, labeled nums[0] to nums[nums.length - 1],
+There is an undirected edge between nums[i] and nums[j] if nums[i] and nums[j] share a common factor greater than 1.
+Return the size of the largest connected component in the graph.
+*/
