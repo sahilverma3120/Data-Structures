fc0bd0afc071084f0750a837059e9e9ee6a3d9c3
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-11-26 23:04:23 +0530

    Update Search Insert Position .java

--- Leetcode Daily/Search Insert Position .java
+++ Leetcode Daily/Search Insert Position .java
@@ -5,3 +5,26 @@ Share
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.*/
+class Solution {
+ public int searchInsert(int[] nums, int target){
+		int min = 0;
+		int max = nums.length;
+		return binaryS(min, max, nums, target);
+	}
+
+public int binaryS(int min, int max, int[] nums, int target){
+    int count = min + (max-min)/2;
+    if (max <= min){
+        return max;
+    }
+    if (nums[count] == target){
+        return count;
+    }
+    else if(nums[count] < target){
+        return binaryS(count+1, max, nums, target);
+    }
+    else{
+        return binaryS(min, count, nums, target);
+    }
+}
+}
