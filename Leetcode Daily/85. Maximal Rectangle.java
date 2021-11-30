ff40242e93343b209ea7109b952d46f5b8a48e84
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-11-30 18:59:03 +0530

    Create 85. Maximal Rectangle.java

---
+++ Leetcode Daily/85. Maximal Rectangle.java
@@ -0,0 +1,3 @@
+//
+//Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
