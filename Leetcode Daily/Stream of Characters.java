15c2fa50c1f08023133d9e6f9e4ae4d22ce2181f
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-12-04 23:49:52 +0530

    Create Stream of Characters.java

---
+++ Leetcode Daily/Stream of Characters.java
@@ -0,0 +1,49 @@
+class StreamChecker {
+    
+    class TrieNode{
+        boolean isWord;
+        TrieNode[] next = new TrieNode[26];
+        
+    }
+TrieNode root = new TrieNode();
+        StringBuilder sb = new StringBuilder();
+    public StreamChecker(String[] words) {
+        for(int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
+    }
+   public boolean query(char letter){
+       sb.append(letter);
+      TrieNode node = root;
+       for(int i=sb.length()-1; i>=0; i--){
+           char ch = sb.charAt(i);
+           node = node.next[ch - 'a'];
+       if(node != null && node.isWord ){
+           return true;
+       }
+       if(node == null){
+           return false;
+       }
+       
+   }
+       return false;
+   }
+          private void insert(String s) {
+            TrieNode node = root;
+            int len = s.length();
+            for (int i = len - 1; i >= 0; i--) {
+                char c = s.charAt(i);
+                if (node.next[c - 'a'] == null) {
+                    node.next[c - 'a'] = new TrieNode();
+                }
+                node = node.next[c - 'a'];
+            }
+            node.isWord = true;
+    } 
+}
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker obj = new StreamChecker(words);
+ * boolean param_1 = obj.query(letter);
+ */
