3451a33cdde64bb1fec6f855d02bbc0ed3722cb2
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-12-08 19:22:43 +0530

    Create Binary Tree Tilt.java

---
+++ Leetcode Daily/Binary Tree Tilt.java
@@ -0,0 +1,19 @@
+Given the root of a binary tree, return the sum of every tree node's tilt.
+
+The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if there the node does not have a right child.
+  
+  /*
+   * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+  */
