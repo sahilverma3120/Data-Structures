eb7961bb839f240e3e5ef74e48f546859f5fdb90
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-12-19 18:25:56 +0530

    Update Decode String.java

--- Leetcode Daily/Decode String.java
+++ Leetcode Daily/Decode String.java
@@ -7,3 +7,25 @@ You may assume that the input string is always valid; No extra white spaces, squ
 
 Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
 */
+private int pos = 0;
+    public String decodeString(String s) {
+    	int n = s.length(), repeat = 0;
+    	StringBuilder buf = new StringBuilder();
+    	while (pos < n) {
+    		char c = s.charAt(pos);
+    		if (c >= 'a' && c <= 'z') {
+    			buf.append(c);
+    		} else if (c >= '0' && c <= '9') {
+    			repeat = repeat * 10 + (c - '0');
+    		} else if (c == '[') {
+    			pos++;  // skip the char '['
+    			String str = decodeString(s);  // pos is diff for each call
+    			for (int i = 0; i < repeat; i++)
+   					buf.append(str);
+    			repeat = 0;  // reset the value for the next input
+    		} else if (c == ']')
+    			break;
+    		pos++;
+    	}
+    	return buf.toString();
+    }
