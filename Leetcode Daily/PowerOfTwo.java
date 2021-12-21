dc73029d78d3df871a03e0d27f7f59a94dc8dadd
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-12-21 22:09:36 +0530

    Update PowerOfTwo.java

--- Leetcode Daily/PowerOfTwo.java
+++ Leetcode Daily/PowerOfTwo.java
@@ -1,3 +1,10 @@
+/*
+
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+*/
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
     if(n<=0)
