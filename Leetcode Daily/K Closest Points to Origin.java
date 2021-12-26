b8f17125bdf147ffb80258fbe7545b7c2fc8929c
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-12-26 22:16:38 +0530

    Update K Closest Points to Origin.java

--- Leetcode Daily/K Closest Points to Origin.java
+++ Leetcode Daily/K Closest Points to Origin.java
@@ -1,4 +1,4 @@
-
+/*
 973. K Closest Points to Origin
 
 Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
@@ -6,5 +6,17 @@ Given an array of points where points[i] = [xi, yi] represents a point on the X-
 The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
 
 You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+ */
+ class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1]));
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            ans[i][0] = points[i][0];
+            ans[i][1] = points[i][1];
+        }
+        return ans;
+        }
+    }
 
  
