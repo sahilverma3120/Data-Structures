7a00b9db1d8331afca8c71ff3ed83340d5c922b1
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2022-01-30 20:23:58 +0530

    Update Rotate Array.java

--- Leetcode Daily/Rotate Array.java
+++ Leetcode Daily/Rotate Array.java
@@ -1,3 +1,21 @@
+/*
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+*/
+
+
 class Solution {
     public static void reverse(int nums[], int i, int j){
         int li = i, ri = j;
