353a4f4517dfa1394f9cfcc59cc8d01ae6e73191
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2022-02-01 00:00:40 +0530

    Update vv.java

--- Leetcode Daily/vv.java
+++ Leetcode Daily/vv.java
@@ -1,1 +1,21 @@
-1
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+       int n = accounts.length;
+      int m = accounts[0].length;
+        
+        int maxWealth = Integer.MIN_VALUE;
+        
+        for(int i=0;  i<n; i++){
+            int customerWealth = 0;
+            for(int j=0; j<m; j++){
+              customerWealth +=  accounts[i][j];
+                if(customerWealth > maxWealth){
+                 maxWealth = customerWealth;
+                }
+            }
+            
+          }
+        return maxWealth;
+    }
+}
